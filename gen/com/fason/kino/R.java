d2eba693b18fc68b4a1c51d36439053b9cb91381
Thomas Holden <tho068@post.uit.no>
2014-08-03 03:02:04 +0200

    Initial

--- gen/com/fason/kino/R.java
+++ gen/com/fason/kino/R.java
@@ -25,7 +25,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
+        public static final int action_settings=0x7f080007;
+        public static final int image=0x7f080006;
         public static final int imageView1=0x7f080003;
         public static final int list_subtitle=0x7f080004;
         public static final int list_title=0x7f080002;
--- src/com/fason/kino/MovieAdapter.java
+++ src/com/fason/kino/MovieAdapter.java
@@ -1,18 +1,81 @@
 package com.fason.kino;
 
-import java.util.List;
-import java.util.Map;
+import com.androidquery.AQuery;
 
+import android.app.Activity;
 import android.content.Context;
-import android.widget.SimpleAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MovieAdapter extends SimpleAdapter {
-
-	public MovieAdapter(Context context, List<? extends Map<String, ?>> data, int resource, String[] from, int[] to) {
-		super(context, data, resource, from, to);
-		// TODO Auto-generated constructor stub
+public class MovieAdapter extends BaseAdapter {
+	String[] label;
+	String[] image;
+	String[] subtitle;
+	private Context context;
+	
+	public MovieAdapter(Context context,String[] label,String[] image, String[] subtitle)
+	{
+	    this.context=context;
+	    this.image = image;
+	    this.label = label;
+	    this.subtitle = subtitle;
+	
+	}
+	
+	private class ViewHolder{
+	    ImageView img;
+	    TextView label;
+		TextView subtitle;
+	}
+	
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+	    ViewHolder holder = null;
+	
+	    LayoutInflater mInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+	    if (convertView == null) {
+	        convertView = mInflater.inflate(R.layout.viewliste, null);
+	        holder = new ViewHolder();
+	        holder.label=(TextView) convertView.findViewById(R.id.list_title);
+	        holder.subtitle=(TextView) convertView.findViewById(R.id.list_subtitle);
+	        holder.img = (ImageView) convertView.findViewById(R.id.image);
+	        convertView.setTag(holder);
+	    }
+	    else {
+	        holder = (ViewHolder) convertView.getTag();
+	    }
+	
+	     AQuery aq = new AQuery(convertView);
+	
+	     aq.id(holder.label).text(label[position]);
+	     aq.id(holder.subtitle).text(label[position]);
+	     aq.id(holder.img).image(image[position], true, true, 0, 0, null, AQuery.FADE_IN_NETWORK, 1.0f);
+	
+	    return convertView;
 	}
 	
 	
-
+	@Override
+	    public int getCount() {
+	        // TODO Auto-generated method stub
+	        return image.length;
+	    }
+	
+	
+	    @Override
+	    public long getItemId(int position) {
+	        // TODO Auto-generated method stub
+	        return position;
+	    }
+	
+	
+	    @Override
+	    public Object getItem(int position) {
+	        // TODO Auto-generated method stub
+	        return null;
+	    }
 }

9daeffbd934997ea81d2d612cf349fe35acc579f
Thomas Holden <tho068@post.uit.no>
2014-08-07 01:35:18 +0200

    Connectivity check

--- src/com/fason/kino/MainActivity.java
+++ src/com/fason/kino/MainActivity.java
@@ -19,7 +19,10 @@ import org.jsoup.select.Elements;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.format.Time;
@@ -34,6 +37,7 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	
@@ -61,138 +65,154 @@ public class MainActivity extends Activity {
 			/* Set listview hidden while loading from server */
 			listview.setVisibility(View.INVISIBLE);
 			spinner.setVisibility(View.VISIBLE);
+			if(isOnline() == false){
+				spinner.setVisibility(View.INVISIBLE);
+			}
 		}
 		
 		@Override
 		protected List doInBackground(Void... params) {
-			// Get all the movies from aurorakino and return a list of them
-			// to the postexecute method.
-			
-			// Get current date
-			Calendar calendar = Calendar.getInstance();
-			Date today = calendar.getTime();
-			calendar.add(Calendar.DAY_OF_YEAR, 1);
-			Date tomorrow = calendar.getTime();
-			
-			String date = new SimpleDateFormat("dd-MM-yyyy").format(today);
-			
-			Hashtable<String, Elements> map = new Hashtable<String, Elements>();
-			
-			try {
-				Document doc = (Document) Jsoup.connect("http://fokus.aurorakino.no/billetter-og-program/?D=" + date).get();
+			// Check if Internet is present
+			if(isOnline() == true){
+				// Get current date
+				Calendar calendar = Calendar.getInstance();
+				Date today = calendar.getTime();
+				calendar.add(Calendar.DAY_OF_YEAR, 1);
+				Date tomorrow = calendar.getTime();
 				
-				Elements test = doc.getElementsByClass("showing");
+				String date = new SimpleDateFormat("dd-MM-yyyy").format(today);
 				
-				/* No movies today - get movies for tomorrow */
-				if(test.size() == 0){
-					MainActivity.today = false;
-					date = new SimpleDateFormat("dd-MM-yyyy").format(tomorrow);
-					doc = (Document) Jsoup.connect("http://fokus.aurorakino.no/billetter-og-program/?D=" + date).get();
-					test = doc.getElementsByClass("showing");
-				}
+				Hashtable<String, Elements> map = new Hashtable<String, Elements>();
 				
-				// Store movieprogram date
-				MainActivity.date = date;
-				
-				List<Object> listofmovies = new ArrayList<Object>();
-		
-				// Iterate over HTML tags and fetch needed data
-				Iterator<Element> iter = test.iterator();
-				while(iter.hasNext()){
-					Element tag = (Element) iter.next();
+				try {
+					Document doc = (Document) Jsoup.connect("http://fokus.aurorakino.no/billetter-og-program/?D=" + date).get();
 					
-					Map<String, Object> movie = new Hashtable<String, Object>();
-					List<String> time = new ArrayList<String>();
+					Elements test = doc.getElementsByClass("showing");
 					
-					// Map data for later usage
-					movie.put("title", tag.select(".movieTitle").text());
-					movie.put("url", "http://fokus.aurorakino.no" + tag.select(".movieTitle").attr("href"));
-					movie.put("desc", tag.select(".movieDescription").text());
-					movie.put("image", "http://fokus.aurorakino.no" + tag.select(".smallPoster").attr("src"));
-					
-					Elements times = tag.select(".programTime");
-					Iterator<Element> itertimes = times.iterator();
-					// Get movietimes
-					while(itertimes.hasNext()){
-						Element movietime = (Element) itertimes.next();
-						time.add(movietime.text());
+					/* No movies today - get movies for tomorrow */
+					if(test.size() == 0){
+						MainActivity.today = false;
+						date = new SimpleDateFormat("dd-MM-yyyy").format(tomorrow);
+						doc = (Document) Jsoup.connect("http://fokus.aurorakino.no/billetter-og-program/?D=" + date).get();
+						test = doc.getElementsByClass("showing");
 					}
 					
-					movie.put("time", time);
+					// Store movieprogram date
+					MainActivity.date = date;
 					
-					listofmovies.add(movie);
+					List<Object> listofmovies = new ArrayList<Object>();
+			
+					// Iterate over HTML tags and fetch needed data
+					Iterator<Element> iter = test.iterator();
+					while(iter.hasNext()){
+						Element tag = (Element) iter.next();
+						
+						Map<String, Object> movie = new Hashtable<String, Object>();
+						List<String> time = new ArrayList<String>();
+						
+						// Map data for later usage
+						movie.put("title", tag.select(".movieTitle").text());
+						movie.put("url", "http://fokus.aurorakino.no" + tag.select(".movieTitle").attr("href"));
+						movie.put("desc", tag.select(".movieDescription").text());
+						movie.put("image", "http://fokus.aurorakino.no" + tag.select(".smallPoster").attr("src"));
+						
+						Elements times = tag.select(".programTime");
+						Iterator<Element> itertimes = times.iterator();
+						// Get movietimes
+						while(itertimes.hasNext()){
+							Element movietime = (Element) itertimes.next();
+							time.add(movietime.text());
+						}
+						
+						movie.put("time", time);
+						
+						listofmovies.add(movie);
+					}
+					
+					return listofmovies;
+				} 
+				catch (IOException e) {
+				// TODO Auto-generated catch block
+					MainActivity.out("Noe gikk galt");
 				}
-				
-				return listofmovies;
-			} 
-			catch (IOException e) {
-			// TODO Auto-generated catch block
-				MainActivity.out("Noe gikk galt");
 			}
 			
+			// Return null if no internet connection
 			return null;
 		}
 		
 		protected void onPostExecute(final List movielist){
-			List<Map<String, String>> list = new ArrayList<Map<String, String>>();
 			
-			List<String> title = new ArrayList<String>();
-			List<String> subtitle = new ArrayList<String>();
-			List<String> image = new ArrayList<String>();
-			List<String> timelist = new ArrayList<String>();
-			
-			// Set subtitle
-			if(MainActivity.today == false){
-				getActionBar().setSubtitle("Kinoprogram " + MainActivity.date);
+			// Print error if no network
+			if(movielist == null){
+				Toast.makeText(getBaseContext(), "Ingen nettverkstilkobling tilgjengelig", Toast.LENGTH_LONG).show();
 			}
 			else {
-				getActionBar().setSubtitle("Kinoprogram " + MainActivity.date);
-			}
-			listview.setClickable(true);
-			listview.setOnItemClickListener(new OnItemClickListener(){
-
-				@Override
-				public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-					Map moviemap = (Map) movielist.get(position);
-					String url = (String) moviemap.get("url");
-					String title = (String) moviemap.get("title");
+				List<Map<String, String>> list = new ArrayList<Map<String, String>>();
+				List<String> title = new ArrayList<String>();
+				List<String> subtitle = new ArrayList<String>();
+				List<String> image = new ArrayList<String>();
+				List<String> timelist = new ArrayList<String>();
+				
+				// Set subtitle
+				if(MainActivity.today == false){
+					getActionBar().setSubtitle("Kinoprogram " + MainActivity.date);
+				}
+				else {
+					getActionBar().setSubtitle("Kinoprogram " + MainActivity.date);
+				}
+				listview.setClickable(true);
+				listview.setOnItemClickListener(new OnItemClickListener(){
+	
+					@Override
+					public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+						Map moviemap = (Map) movielist.get(position);
+						String url = (String) moviemap.get("url");
+						String title = (String) moviemap.get("title");
+						
+						Intent intent = new Intent(MainActivity.this, SingleView.class);
+						intent.putExtra("url", url);
+						intent.putExtra("title", title);
+						startActivity(intent);
+					}
+				});
+				// Iterate over list of movies
+				Iterator movieiter = movielist.iterator();
+				while(movieiter.hasNext()){
+					Map map = (Hashtable) movieiter.next();
+				
+					String time = new String();
 					
-					Intent intent = new Intent(MainActivity.this, SingleView.class);
-					intent.putExtra("url", url);
-					intent.putExtra("title", title);
-					startActivity(intent);
+					title.add((String) map.get("title"));
+					subtitle.add((String) map.get("desc"));
+					image.add((String) map.get("image"));
+					
+					List timetime = (List) map.get("time");
+					Iterator timeiter = timetime.iterator();
+					time = (String) timeiter.next();
+					while(timeiter.hasNext()){
+						time = time + "," + timeiter.next();
+					}
+					
+					// Format the text better
+					time = time.replaceAll("(3D)", "");
+					time = time.replaceAll("[^\\d:,]", "");
+					time = time.replace(",", ", ");
+					
+					timelist.add(time);
 				}
-			});
-			// Iterate over list of movies
-			Iterator movieiter = movielist.iterator();
-			while(movieiter.hasNext()){
-				Map map = (Hashtable) movieiter.next();
-			
-				String time = new String();
-				
-				title.add((String) map.get("title"));
-				subtitle.add((String) map.get("desc"));
-				image.add((String) map.get("image"));
-				
-				List timetime = (List) map.get("time");
-				Iterator timeiter = timetime.iterator();
-				time = (String) timeiter.next();
-				while(timeiter.hasNext()){
-					time = time + "," + timeiter.next();
-				}
-				
-				// Format the text better
-				time = time.replaceAll("(3D)", "");
-				time = time.replaceAll("[^\\d:,]", "");
-				time = time.replace(",", ", ");
-				
-				timelist.add(time);
+				MovieAdapter adapter = new MovieAdapter(getBaseContext(), title, image, subtitle, timelist);
+				listview.setAdapter(adapter);
+				spinner.setVisibility(View.INVISIBLE);
+				listview.setVisibility(View.VISIBLE);	
 			}
-			MovieAdapter adapter = new MovieAdapter(getBaseContext(), title, image, subtitle, timelist);
-			listview.setAdapter(adapter);
-			spinner.setVisibility(View.INVISIBLE);
-			listview.setVisibility(View.VISIBLE);		
 		}
+		
+		public boolean isOnline() {
+		    ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		    NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+		    return (networkInfo != null && networkInfo.isConnected());
+		}  
 	}
 	public static void out(Object msg){
 		Log.i("info", msg.toString());
